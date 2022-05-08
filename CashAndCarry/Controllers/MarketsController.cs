66ccbc8e0bd1cfb7cfd27a73f29cf8036780856d
Bilal <bhw03@mail.aub.edu>
2022-05-09 00:16:09 +0300

    DesignApp

--- CashAndCarry/Controllers/MarketsController.cs
+++ CashAndCarry/Controllers/MarketsController.cs
@@ -7,8 +7,9 @@ namespace CashAndCarry.Controllers
     {
         static List<Market> markets = new List<Market>
         {
-            new Market {Id = 1, Name = "Market1", Open = "9:00-AM", Close = "12:00-AM", Image = "images/super1.png"},
-            new Market {Id = 1, Name = "Market1", Open = "9:00-AM", Close = "12:00-AM", Image = "images/super1.png"}
+            new Market {Id = 1, Name = "Sppinneys", Open = "9:00-AM", Close = "12:00-AM", Image = "images/super1.png"},
+            new Market {Id = 1, Name = "Carffour", Open = "9:00-AM", Close = "12:00-AM", Image = "images/super1.png"},
+            new Market {Id = 1, Name = "Makhazen", Open = "9:00-AM", Close = "12:00-AM", Image = "images/super1.png"}
         };
         public IActionResult Index()
         {
